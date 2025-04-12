cd6c77e8f7fc15307a723813d92705b885bd2ddc
Fabricio Olivetti de Franca <fabricio.olivetti@gmail.com>
2025-04-12 20:23:58 -0300

    Update install_ghc_x86.bash

--- install_ghc_x86.bash
+++ install_ghc_x86.bash
@@ -14,8 +14,8 @@ rm -rf "/tmp/ghc-9.6.5" "/tmp/ghc-9.6.5.tar.xz"
 python3 -c "import wget; wget.download('https://github.com/haskell/cabal/archive/refs/tags/cabal-install-v3.14.2.0.zip', '/tmp/cabal.zip')"
 python3 -c "import shutil; shutil.unpack_archive('/tmp/cabal.zip', '/tmp')"
 mv "/tmp/cabal-cabal-install-v3.14.2.0" "/tmp/cabal"
-sed -ie "s/+ofd-locking/-ofd-locking/" "/tmp/cabal/bootstrap/linux-9.6.5.json"
-(cd "/tmp/cabal" && python3 "./bootstrap/bootstrap.py" -d "./bootstrap/linux-9.6.5.json" -w "/tmp/ghc-toolset-9.6.5/root/usr/bin/ghc-9.6.5")
+#sed -ie "s/+ofd-locking/-ofd-locking/" "/tmp/cabal/bootstrap/linux-9.6.5.json"
+#(cd "/tmp/cabal" && python3 "./bootstrap/bootstrap.py" -d "./bootstrap/linux-9.6.5.json" -w "/tmp/ghc-toolset-9.6.5/root/usr/bin/ghc-9.6.5")
 PATH="/tmp/ghc-toolset-9.6.5/root/usr/bin:${PATH}" "/tmp/cabal/_build/bin/cabal" v2-update
 PATH="/tmp/ghc-toolset-9.6.5/root/usr/bin:${PATH}" "/tmp/cabal/_build/bin/cabal" v2-install cabal-install --constraint="lukko -ofd-locking" --overwrite-policy=always --install-method=copy --installdir="/tmp/ghc-toolset-9.6.5/root/usr/bin"
 
@@ -23,8 +23,8 @@ PATH="/tmp/ghc-toolset-9.6.5/root/usr/bin:${PATH}" "/tmp/cabal/_build/bin/cabal"
 python3 -c "import wget; wget.download('https://downloads.haskell.org/~ghc/9.12.1/ghc-9.12.1-src.tar.xz', '/tmp/ghc-9.12.1-src.tar.xz')"
 python3 -c "import shutil; shutil.unpack_archive('/tmp/ghc-9.12.1-src.tar.xz', '/tmp')"
 
-(cd "/tmp" && patch -p0 < "/tmp/ghc-9.12.1-patches/fpic-default.patch")
-rm -rf "/tmp/ghc-9.12.1-patches"
+#(cd "/tmp" && patch -p0 < "/tmp/ghc-9.12.1-patches/fpic-default.patch")
+#rm -rf "/tmp/ghc-9.12.1-patches"
 
 (cd "/tmp/ghc-9.12.1" && PATH="/tmp/ghc-toolset-9.6.5/root/usr/bin:${PATH}" ./configure GHC="/tmp/ghc-toolset-9.6.5/root/usr/bin/ghc-9.6.5")
 (cd "/tmp/ghc-9.12.1" && PATH="/tmp/ghc-toolset-9.6.5/root/usr/bin:${PATH}" hadrian/build install --docs=none -j --prefix="/usr/local")
