7359e7c8ce841661a76a4d9eb48d7ea6f2f5b290
Olivetti <fabricio.olivetti@gmail.com>
2025-04-14 17:16:11 -0300

    docker updated

---
+++ build_manylinux2014_i686.sh
@@ -0,0 +1,1 @@
+docker buildx build --tag folivetti/manylinux2014_ghc9121_i686 --file Dockerfile.manylinux2014_ghc9121_i686 . --no-cache
---
+++ build_manylinux_2_28_x86.sh
@@ -0,0 +1,1 @@
+docker buildx build --tag folivetti/manylinux_2_28_ghc9121_x86_64 --file Dockerfile.manylinux_2_28_ghc9121_x86_64 . --no-cache
